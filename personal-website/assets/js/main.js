fd9ab046409755805e7aca39a0d3fb71e3ba8792
Eric Fletcher <EricFletcher3@gmail.com>
2021-08-21 16:04:49 -0400

    Replaced project files to unmask photo

--- personal-website/assets/js/main.js
+++ personal-website/assets/js/main.js
@@ -28,7 +28,6 @@ function linkAction() {
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
-
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
